e5cf737499cea03616cfa7c3b92de02b3fbefec6
Кирилл <105377400+Cheburek254@users.noreply.github.com>
2025-11-26 20:54:13 +0300

    Update square.py

--- square.py
+++ square.py
@@ -3,7 +3,7 @@ import unittest
 
 class SquareTestCase(unittest.TestCase):
     def test_zero_mul(self):
-        res = area(10,0)
+        res = area(0)
         self.assertEqual(res, 0)
     def test_square(self):
         res = area(10)
