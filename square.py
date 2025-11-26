276a9e5630f77da3f972099bce2fd9e755d66f71
Кирилл <105377400+Cheburek254@users.noreply.github.com>
2025-11-26 20:52:42 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,12 +1,12 @@
 import unittest
 
 
-class RectangleTestCase(unittest.TestCase):
+class SquareTestCase(unittest.TestCase):
     def test_zero_mul(self):
         res = area(10,0)
         self.assertEqual(res, 0)
     def test_square(self):
-        res = area(10,10)
+        res = area(10)
         self.assertEqual(res, 100)
     def test_perimeter(self):
         res = perimeter(10)
